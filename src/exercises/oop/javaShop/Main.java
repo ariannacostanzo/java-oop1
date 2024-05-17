ecd60bf61b02bd214de44f69886c9b5bb23c1796
ariannacostanzo <costanzo.arianna@outlook.it>
2024-05-17 14:05:08 +0200

    first commit

---
+++ src/exercises/oop/javaShop/Main.java
@@ -0,0 +1,8 @@
+package exercises.oop.javaShop;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/exercises/oop/javaShop/Product.java
@@ -0,0 +1,4 @@
+package exercises.oop.javaShop;
+
+public class Product {
+}
